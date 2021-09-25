119f16ebd2668f1dd3b1ab84ab505ed35cbb59c2
resonantia <resonantia@clutte.red>
2021-09-25 16:24:38 +0900

    Revise `HAND_NAME` to enum `HandKind`.

--- src/hand_info/winning_hand.rs
+++ src/hand_info/winning_hand.rs
@@ -2,6 +2,8 @@ use crate::board::Rules;
 use crate::hand_info::hand_analyzer::HandAnalyzer;
 use crate::hand_info::status::Status;
 use crate::tile::{Dragon, Tile};
+use strum::{EnumCount, IntoEnumIterator};
+use strum_macros::{EnumCount as EnumCountMacro, EnumIter};
 
 /// 役を判定する
 use std::collections::HashMap;
@@ -19,238 +21,245 @@ pub enum WinningHandForm {
     Normal,
 }
 
-/// 役の名前
+/// 役を表す列挙型
 ///
 /// <https://en.wikipedia.org/wiki/Japanese_Mahjong_yaku>による英語名
-pub const HAND_NAME: [&'static str; 42] = [
-    // 立直
-    "ready_hand",
-    // 七対子
-    "seven_pairs",
-    // 流し満貫
-    "nagashi_mangan",
-    // 門前清自摸和
-    "self_pick",
-    // 一発
-    "one_shot",
-    // 海底撈月
-    "last_tile_from_the_wall",
-    // 河底撈魚
-    "last_discard",
-    // 嶺上開花
-    "dead_wall_draw",
-    // 搶槓
-    "robbing_a_quad",
-    // ダブル立直
-    "double_ready",
-    // 平和
-    "no_points_hand",
-    // 一盃口
-    "one_set_of_identical_sequences",
-    // 三色同順
-    "three_colour_straight",
-    // 一気通貫
-    "straight",
-    // 二盃口
-    "two_sets_of_identical_sequences",
-    // 対々和
-    "all_triplet_hand",
-    // 三暗刻
-    "three_closed_triplets",
-    // 三色同刻
-    "three_colour_triplets",
-    // 断么九
-    "all_simples",
-    // 役牌（自風牌）
-    "honor_tiles_players_wind",
-    // 役牌（場風牌）
-    "honor_tiles_prevailing_wind",
-    // 役牌（白）
-    "honor_tiles_white_dragon",
-    // 役牌（發）
-    "honor_tiles_green_dragon",
-    // 役牌（中）
-    "honor_tiles_red_dragon",
-    // 混全帯么九
-    "terminal_or_honor_in_each_set",
-    // 純全帯么九
-    "terminal_in_each_set",
-    // 混老頭
-    "all_terminals_and_honors",
-    // 小三元
-    "little_three_dragons",
-    // 混一色
-    "half_flush",
-    // 清一色
-    "flush",
-    // 国士無双
-    "thirteen_orphans",
-    // 四暗刻
-    "four_concealed_triplets",
-    // 大三元
-    "big_three_dragons",
-    // 小四喜
-    "little_four_winds",
-    // 大四喜
-    "big_four_winds",
-    // 字一色
-    "all_honors",
-    // 清老頭
-    "all_terminals",
-    // 緑一色
-    "all_green",
-    // 九蓮宝燈
-    "nine_gates",
-    // 四槓子
-    "four_kans",
-    // 天和
-    "heavenly_hand",
-    // 地和
-    "hand_of_earth",
-];
+#[derive(Debug, PartialEq, Eq, Hash, EnumCountMacro, EnumIter)]
+pub enum HandKind {
+    /// 立直
+    ReadyHand,
+    /// 七対子
+    SevenPairs,
+    /// 流し満貫
+    NagashiMangan,
+    /// 門前清自摸和
+    SelfPick,
+    /// 一発
+    OneShot,
+    /// 海底撈月
+    LastTileFromTheWall,
+    /// 河底撈魚
+    LastDiscard,
+    /// 嶺上開花
+    DeadWallDraw,
+    /// 搶槓
+    RobbingAQuad,
+    /// ダブル立直
+    DoubleReady,
+    /// 平和
+    NoPointsHand,
+    /// 一盃口
+    OneSetOfIdenticalSequences,
+    /// 三色同順
+    ThreeColourStraight,
+    /// 一気通貫
+    Straight,
+    /// 二盃口
+    TwoSetsOfIdenticalSequences,
+    /// 対々和
+    AllTripletHand,
+    /// 三暗刻
+    ThreeClosedTriplets,
+    /// 三色同刻
+    ThreeColourTriplets,
+    /// 断么九
+    AllSimples,
+    /// 役牌（自風牌）
+    HonorTilesPlayersWind,
+    /// 役牌（場風牌）
+    HonorTilesPrevailingWind,
+    /// 役牌（白）
+    HonorTilesWhiteDragon,
+    /// 役牌（發）
+    HonorTilesGreenDragon,
+    /// 役牌（中）
+    HonorTilesRedDragon,
+    /// 混全帯么九
+    TerminalOrHonorInEachSet,
+    /// 純全帯么九
+    TerminalInEachSet,
+    /// 混老頭
+    AllTerminalsAndHonors,
+    /// 小三元
+    LittleThreeDragons,
+    /// 混一色
+    HalfFlush,
+    /// 清一色
+    Flush,
+    /// 国士無双
+    ThirteenOrphans,
+    /// 四暗刻
+    FourConcealedTriplets,
+    /// 大三元
+    BigThreeDragons,
+    /// 小四喜
+    LittleFourWinds,
+    /// 大四喜
+    BigFourWinds,
+    /// 字一色
+    AllHonors,
+    /// 清老頭
+    AllTerminals,
+    /// 緑一色
+    AllGreen,
+    /// 九蓮宝燈
+    NineGates,
+    /// 四槓子
+    FourKans,
+    /// 天和
+    HeavenlyHand,
+    /// 地和
+    HandOfEarth,
+}
 
 pub fn check<'a, 'b>(
     hand: &'a HandAnalyzer,
     status: &'b Status,
     rules: &'b Rules,
-) -> HashMap<&'a str, (&'static str, bool, u32)> {
-    let mut result = HashMap::with_capacity(HAND_NAME.len());
-    for i in 0..HAND_NAME.len() {
-        result.insert(HAND_NAME[i], ("unknown", false, 0));
+) -> HashMap<HandKind, (&'static str, bool, u32)> {
+    let mut result = HashMap::with_capacity(HandKind::COUNT);
+    for hand_kind in HandKind::iter() {
+        result.insert(hand_kind, ("Unknown", false, 0));
     }
 
     // 立直
-    result.insert("ready_hand", check_ready_hand(hand, status));
+    result.insert(HandKind::ReadyHand, check_ready_hand(hand, status));
     // 七対子
-    result.insert("seven_pairs", check_seven_pairs(hand));
+    result.insert(HandKind::SevenPairs, check_seven_pairs(hand));
     // 流し満貫
-    result.insert("nagashi_mangan", check_nagashi_mangan(hand, status));
+    result.insert(HandKind::NagashiMangan, check_nagashi_mangan(hand, status));
     // 門前清自摸和
-    result.insert("self_pick", check_self_pick(hand, status));
+    result.insert(HandKind::SelfPick, check_self_pick(hand, status));
     // 一発
-    result.insert("one_shot", check_one_shot(hand, status));
+    result.insert(HandKind::OneShot, check_one_shot(hand, status));
     // 海底撈月
     result.insert(
-        "last_tile_from_the_wall",
+        HandKind::LastTileFromTheWall,
         check_last_tile_from_the_wall(hand, status),
     );
     // 河底撈魚
-    result.insert("last_discard", check_last_discard(hand, status));
+    result.insert(HandKind::LastDiscard, check_last_discard(hand, status));
     // 嶺上開花
-    result.insert("dead_wall_draw", check_dead_wall_draw(hand, status));
+    result.insert(HandKind::DeadWallDraw, check_dead_wall_draw(hand, status));
     // 搶槓
-    result.insert("robbing_a_quad", check_robbing_a_quad(hand, status));
+    result.insert(HandKind::RobbingAQuad, check_robbing_a_quad(hand, status));
     // ダブル立直
-    result.insert("double_ready", check_double_ready(hand, status));
+    result.insert(HandKind::DoubleReady, check_double_ready(hand, status));
     // 平和
-    result.insert("no_points_hand", check_no_points_hand(hand, status));
+    result.insert(HandKind::NoPointsHand, check_no_points_hand(hand, status));
     // 一盃口
     result.insert(
-        "one_set_of_identical_sequences",
+        HandKind::OneSetOfIdenticalSequences,
         check_one_set_of_identical_sequences(hand, status),
     );
     // 三色同順
     result.insert(
-        "three_colour_straight",
+        HandKind::ThreeColourStraight,
         check_three_colour_straight(hand, status),
     );
     // 一気通貫
-    result.insert("straight", check_straight(hand, status));
+    result.insert(HandKind::Straight, check_straight(hand, status));
     // 二盃口
     result.insert(
-        "two_sets_of_identical_sequences",
+        HandKind::TwoSetsOfIdenticalSequences,
         check_two_sets_of_identical_sequences(hand, status),
     );
     // 対々和
-    result.insert("all_triplet_hand", check_all_triplet_hand(hand));
+    result.insert(HandKind::AllTripletHand, check_all_triplet_hand(hand));
     // 三暗刻
     result.insert(
-        "three_closed_triplets",
+        HandKind::ThreeClosedTriplets,
         check_three_closed_triplets(hand, status),
     );
     // 三色同刻
     result.insert(
-        "three_colour_triplets",
+        HandKind::ThreeColourTriplets,
         check_three_colour_triplets(hand, status),
     );
     // 断么九
-    result.insert("all_simples", check_all_simples(hand, status, rules));
+    result.insert(HandKind::AllSimples, check_all_simples(hand, status, rules));
     // 役牌（自風牌）
     result.insert(
-        "honor_tiles_players_wind",
+        HandKind::HonorTilesPlayersWind,
         check_honor_tiles_players_wind(hand, status),
     );
     // 役牌（場風牌）
     result.insert(
-        "honor_tiles_prevailing_wind",
+        HandKind::HonorTilesPrevailingWind,
         check_honor_tiles_prevailing_wind(hand, status),
     );
     // 役牌（白）
     result.insert(
-        "honor_tiles_white_dragon",
+        HandKind::HonorTilesWhiteDragon,
         check_honor_tiles_white_dragon(hand),
     );
     // 役牌（發）
     result.insert(
-        "honor_tiles_green_dragon",
+        HandKind::HonorTilesGreenDragon,
         check_honor_tiles_green_dragon(hand),
     );
     // 役牌（中）
     result.insert(
-        "honor_tiles_red_dragons",
+        HandKind::HonorTilesRedDragon,
         check_honor_tiles_red_dragon(hand),
     );
     // 混全帯么九
     result.insert(
-        "terminal_or_honor_in_each_set",
+        HandKind::TerminalOrHonorInEachSet,
         check_terminal_or_honor_in_each_set(hand, status),
     );
-    // 純全帯么九
     result.insert(
-        "terminal_in_each_set",
+        HandKind::TerminalInEachSet,
         check_terminal_in_each_set(hand, status),
     );
     // 混老頭
     result.insert(
-        "all_terminals_and_honors",
+        HandKind::AllTerminalsAndHonors,
         check_all_terminals_and_honors(hand, status),
     );
     // 小三元
     result.insert(
-        "little_three_dragons",
+        HandKind::LittleThreeDragons,
         check_little_three_dragons(hand, status),
+        // 純全帯么九
     );
     // 混一色
-    result.insert("half_flush", check_half_flush(hand, status));
+    result.insert(HandKind::HalfFlush, check_half_flush(hand, status));
     // 清一色
-    result.insert("flush", check_flush(hand, status));
+    result.insert(HandKind::Flush, check_flush(hand, status));
     // 国士無双
-    result.insert("thirteen_orphans", check_thirteen_orphans(hand));
+    result.insert(HandKind::ThirteenOrphans, check_thirteen_orphans(hand));
     // 四暗刻
     result.insert(
-        "four_concealed_triplets",
+        HandKind::FourConcealedTriplets,
         check_four_concealed_triplets(hand, status),
     );
     // 大三元
-    result.insert("big_three_dragons", check_big_three_dragons(hand, status));
+    result.insert(
+        HandKind::BigThreeDragons,
+        check_big_three_dragons(hand, status),
+    );
     // 小四喜
-    result.insert("little_four_winds", check_little_four_winds(hand, status));
+    result.insert(
+        HandKind::LittleFourWinds,
+        check_little_four_winds(hand, status),
+    );
     // 大四喜
-    result.insert("big_four_winds", check_big_four_winds(hand, status));
+    result.insert(HandKind::BigFourWinds, check_big_four_winds(hand, status));
     // 字一色
-    result.insert("all_honors", check_all_honors(hand, status));
+    result.insert(HandKind::AllHonors, check_all_honors(hand, status));
     // 清老頭
-    result.insert("all_terminals", check_all_terminals(hand, status));
+    result.insert(HandKind::AllTerminals, check_all_terminals(hand, status));
     // 緑一色
-    result.insert("all_green", check_all_green(hand, status));
+    result.insert(HandKind::AllGreen, check_all_green(hand, status));
     // 九蓮宝燈
-    result.insert("nine_gates", check_nine_gates(hand, status));
+    result.insert(HandKind::NineGates, check_nine_gates(hand, status));
     // 四槓子
-    result.insert("four_kans", check_four_kans(hand, status));
+    result.insert(HandKind::FourKans, check_four_kans(hand, status));
     // 天和
-    result.insert("heavenly_hand", check_heavenly_hand(hand, status));
+    result.insert(HandKind::HeavenlyHand, check_heavenly_hand(hand, status));
     // 地和
-    result.insert("hand_of_earth", check_hand_of_earth(hand, status));
+    result.insert(HandKind::HandOfEarth, check_hand_of_earth(hand, status));
 
     return result;
 }
