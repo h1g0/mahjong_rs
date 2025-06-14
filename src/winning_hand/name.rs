da195538b7a6207524c134fc978b44b37934db1f
hi_go <me@clutte.red>
2025-06-14 18:21:07 +0900

    Fix doctest imports

--- src/winning_hand/name.rs
+++ src/winning_hand/name.rs
@@ -114,8 +114,8 @@ pub enum Kind {
 /// # Examples
 ///
 /// ```
-/// use mahjong_rs::settings::Lang;
-/// use mahjong_rs::winning_hand::name::*;
+/// use riichi_mahjong_rs::settings::Lang;
+/// use riichi_mahjong_rs::winning_hand::name::*;
 ///
 /// assert_eq!(get(Kind::ThreeColourStraight, true, Lang::Ja), "三色同順（鳴）");
 /// assert_eq!(get(Kind::ThreeColourStraight, false, Lang::Ja), "三色同順");
